1bedc35ab0715f73d89dc3accdb968aed7ed8e88
lalatina11 <rahmadancandra@gmail.com>
2025-07-28 22:20:13 +0700

    feat: add insert comment api end point

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -1,7 +1,9 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
 import { eq } from 'drizzle-orm';
-import { commentTable, likeTable } from '$lib/server/db/schema';
+import { commentTable, likeTable, userTable } from '$lib/server/db/schema';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+import { SECRET_KEY } from '$env/static/private';
 
 export const GET = async ({ request }: RequestEvent) => {
 	try {
@@ -32,5 +34,48 @@ export const GET = async ({ request }: RequestEvent) => {
 				status: 400
 			}
 		);
+	}
+};
+
+export const POST = async ({ request, cookies }: RequestEvent) => {
+	try {
+		const formData = await request.formData();
+		const description = formData.get('description') as string;
+		const postId = new URL(request.url).searchParams.get('postId');
+		if (!postId || !+postId || +postId <= 0) {
+			throw new Error('Invalid postId');
+		}
+		const token = cookies.get('user_token') as string;
+		if (!token) {
+			throw new Error('Not Authenticated!');
+		}
+		const payload = jwt.verify(token, SECRET_KEY) as JwtPayload;
+		if (!payload || !payload.id) {
+			throw new Error('Not Authenticated!');
+		}
+		const [newComment] = await db
+			.insert(commentTable)
+			.values({ userId: payload.id, postId: +postId, description })
+			.returning();
+		const [comment] = await db
+			.select({
+				id: commentTable.id,
+				userId: commentTable.userId,
+				description: commentTable.description,
+				user: {
+					id: userTable.id,
+					username: userTable.username,
+					email: userTable.email,
+					avatar: userTable.avatar,
+					firstName: userTable.firstName,
+					lastName: userTable.lastName
+				}
+			})
+			.from(commentTable)
+			.leftJoin(userTable, eq(userTable.id, commentTable.userId))
+			.where(eq(commentTable.id, newComment.id));
+		return json({ error: false, data: comment, message: 'created' }, { status: 201 });
+	} catch (err) {
+		return json({ error: true, data: null, message: (err as Error).message }, { status: 201 });
 	}
 };
